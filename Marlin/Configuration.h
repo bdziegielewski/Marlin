f00dea7301b41c28b843ccc41b747bd89d7d4c94
bdziegielewski <47897311+bdziegielewski@users.noreply.github.com>
2022-11-25 18:08:12 +0100

    FAST PROBING

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1490,13 +1490,13 @@
 #define PROBING_MARGIN 40
 
 // X and Y axis travel speed (mm/min) between probes
-#define XY_PROBE_FEEDRATE (166*60)
+#define XY_PROBE_FEEDRATE (200*60)
 
 // Feedrate (mm/min) for the first approach when double-probing (MULTIPLE_PROBING == 2)
 #define Z_PROBE_FEEDRATE_FAST (15*60)
 
 // Feedrate (mm/min) for the "accurate" probe of each point
-#define Z_PROBE_FEEDRATE_SLOW (Z_PROBE_FEEDRATE_FAST / 4)
+#define Z_PROBE_FEEDRATE_SLOW (Z_PROBE_FEEDRATE_FAST / 5)
 
 /**
  * Probe Activation Switch
